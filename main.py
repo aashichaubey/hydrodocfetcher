3fce716faa886257406e001dd0ea0f24fb6e22f5
Aashi Chaubey <aashic63@gmail.com>
2026-06-28 10:45:25 -0400

    add Resend webhook endpoint

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
@@ -6,3 +7,16 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"status": "Agent is running"}
+
+@app.post("/webhooks/resend")
+async def receive_email(request: Request):
+    event = await request.json()
+    print("Email event received:", event)
+    return {"received": True}
+
+"""
+curl -X POST http://127.0.0.1:8000/webhooks/resend \
+  -H "Content-Type: application/json" \
+  -d '{"type":"email.received","data":{"from":"agent@granuuneav.resend.app"}}'
+
+"""
